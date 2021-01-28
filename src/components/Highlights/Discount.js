b5b9cd8938c7cec5a20ced36ac2cffb83db25543
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2021-01-27 19:12:24 -0500

    added highlights and button component

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,30 +1,45 @@
 import React, { Component } from "react";
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
-const Discount = () => {
-  return (
-    <div className="center_wrapper">
-      <div className="discount_wrapper">
-        <Fade>
-          <div className="discount_porcentage">
-            <span>23%</span>
-            <span>Off</span>
-          </div>
-        </Fade>
-        <Slide right>
-          <div className="discount_description">
-            <h3>Purchase tickets before 2021 ends</h3>
-            <p>
-              San Francisco’s Grammy Award-winning Kronos Quartet brings a
-              program of music from the original seven countries “banned” by the
-              USA — Iran, Iraq, Libya, Somalia, Sudan, Syria, and Yemen
-              —alongside protest music inspired by Pete Seeger.
-            </p>
-          </div>
-        </Slide>
+class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 30,
+  };
+  percentage = () => {
+    if (this.state.discountStart < this.state.discountEnd)
+      this.setState({ discountStart: this.state.discountStart + 1 });
+  };
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.percentage();
+    }, 30);
+  }
+  render() {
+    return (
+      <div className="center_wrapper">
+        <div className="discount_wrapper">
+          <Fade onReveal={() => this.percentage()}>
+            <div className="discount_porcentage">
+              <span>{this.state.discountStart}%</span>
+              <span>Off</span>
+            </div>
+          </Fade>
+          <Slide right>
+            <div className="discount_description">
+              <h3>Purchase tickets before 2021 ends</h3>
+              <p>
+                San Francisco’s Grammy Award-winning Kronos Quartet brings a
+                program of music from the original seven countries “banned” by
+                the USA — Iran, Iraq, Libya, Somalia, Sudan, Syria, and Yemen
+                —alongside protest music inspired by Pete Seeger.
+              </p>
+            </div>
+          </Slide>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Discount;
---
+++ src/components/utils/myButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import TicketIcon from "../../resources/images/icons/ticket.png";
+const MyButton = (props) => {
+  return <Button></Button>;
+};
+
+export default MyButton;
